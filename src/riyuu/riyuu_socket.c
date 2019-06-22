c2f03c9a690d5199c79f4f0fb301c1403374ab59
Ammar Faizi <ammarfaizi2@gmail.com>
2019-06-22 20:55:32 +0700

    Update d

--- src/riyuu/riyuu_socket.c
+++ src/riyuu/riyuu_socket.c
@@ -44,37 +44,37 @@ void riyuu_tcp_bind(char *addr, uint16_t port)
 {
 	socklen_t clilen;
 	int sockfd, newsockfd;
-    struct sockaddr_in serv_addr, cli_addr;
-    threadpool thpool;
+	struct sockaddr_in serv_addr, cli_addr;
+	threadpool thpool;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd < 0) {
-    	error("ERROR opening socket");
-    }
+	if (sockfd < 0) {
+		error("ERROR opening socket");
+	}
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(addr);
-    serv_addr.sin_port = htons(port);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        error("ERROR on binding");
-    }
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(addr);
+	serv_addr.sin_port = htons(port);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR on binding");
+	}
 
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
 
-    printf("Listening on %s:%d...\n", addr, port);
-    thpool = thpool_init(5);
+	printf("Listening on %s:%d...\n", addr, port);
+	thpool = thpool_init(5);
 
-    while (1) {
-		newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-		if (newsockfd < 0) {
-			error("ERROR on accept");
-		}
-		thpool_add_work(thpool, thread_worker, (void *)&newsockfd);
-		close(newsockfd);
-    }
+	while (1) {
+	newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+	if (newsockfd < 0) {
+		error("ERROR on accept");
+	}
+	thpool_add_work(thpool, thread_worker, (void *)&newsockfd);
+	close(newsockfd);
+	}
    
    return;
 }
