1f748a49db0dcc30ccfb5f35d6f57529298d7765
Ammar Faizi <ammarfaizi2@gmail.com>
2019-06-23 20:48:38 +0700

    Update riyuu_client

--- src/riyuu/riyuu_client.c
+++ src/riyuu/riyuu_client.c
@@ -32,9 +32,9 @@ _start_riyuu_shell:
 
 
 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read(sock, buffer, 1024); 
-    printf("%s\n", buffer); 
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
     return 0;
 }
